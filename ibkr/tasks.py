55a1098154dbd459779f35de46a25a524c6f419b
sagar-spark <sagar@sparkbrains.in>
2024-12-05 17:31:37 +0530

    Changes in tickle task

--- ibkr/tasks.py
+++ ibkr/tasks.py
@@ -2,22 +2,27 @@ from celery import shared_task
 import requests
 from django.conf import settings
 
+from core.views import IBKRBase
+
 
 @shared_task
-def tickle_ibkr_session():
+def tickle_ibkr_session(data):
     """
     Task to hit the IBKR tickle API every 2 minutes to maintain the session.
     """
     try:
-        tickle_url = f"{settings.IBKR_BASE_URL}/tickle"
-        response = requests.post(tickle_url, verify=False)
+        ibkr = IBKRBase()
+        response = ibkr.auth_status()
+        if response.get('success'):
+            tickle_url = f"{settings.IBKR_BASE_URL}/tickle"
+            response = requests.post(tickle_url, verify=False)
 
-        if response.status_code == 200:
-            return {"message": "IBKR tickle successful", "status_code": 200}
-        else:
-            return {
-                "message": "Failed to tickle IBKR session",
-                "status_code": response.status_code,
-            }
+            if response.status_code == 200:
+                return {"message": "IBKR tickle successful", "status_code": 200}
+            else:
+                return {
+                    "message": "Failed to tickle IBKR session",
+                    "status_code": response.status_code,
+                }
     except requests.exceptions.RequestException as e:
         return {"error": "Error hitting IBKR tickle endpoint", "details": str(e)}
