f445ff4381255aad0bc0183d2cf4b96aefca458b
sagar-spark <sagar@sparkbrains.in>
2024-12-06 12:33:34 +0530

    Adding code for adding arguments and fetching the onboarding instance in celery task.

--- ibkr/tasks.py
+++ ibkr/tasks.py
@@ -1,8 +1,10 @@
 from celery import shared_task
 import requests
 from django.conf import settings
+from django_celery_beat.models import PeriodicTask
 
 from core.views import IBKRBase
+from ibkr.models import OnBoardingProcess
 
 
 @shared_task
@@ -11,18 +13,33 @@ def tickle_ibkr_session(data):
     Task to hit the IBKR tickle API every 2 minutes to maintain the session.
     """
     try:
+        onboarding_id = data.get('onboarding_id')
+        user_id = data.get('user_id')
+        task_id = data.get('task_id')
+        onboarding_obj = OnBoardingProcess.objects.filter(id=onboarding_id, user_id=user_id).first()
+        auth_fail = False
         ibkr = IBKRBase()
         response = ibkr.auth_status()
         if response.get('success'):
             tickle_url = f"{settings.IBKR_BASE_URL}/tickle"
             response = requests.post(tickle_url, verify=False)
 
-            if response.status_code == 200:
-                return {"message": "IBKR tickle successful", "status_code": 200}
-            else:
+            if not response.status_code == 200:
+                auth_fail = True
                 return {
                     "message": "Failed to tickle IBKR session",
                     "status_code": response.status_code,
                 }
+        else:
+            auth_fail = True
+
+        if auth_fail:
+            onboarding_obj.authenticated = False
+            onboarding_obj.save()
+
+            # disable the task
+            task = PeriodicTask.objects.get(id=task_id)
+            task.enabled = False
+            task.save()
     except requests.exceptions.RequestException as e:
         return {"error": "Error hitting IBKR tickle endpoint", "details": str(e)}
