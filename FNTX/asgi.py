617a21b04c1d2faa641753a68eeed38212d3a2aa
Ubuntu <ubuntu@ip-172-31-36-81.ap-southeast-1.compute.internal>
2024-12-30 07:31:20 +0000

    Removing unwanted code.

--- FNTX/asgi.py
+++ FNTX/asgi.py
@@ -14,12 +14,6 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FNTX.settings")
 django.setup()
 
-import django
-
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FNTX.settings")
-django.setup()
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
