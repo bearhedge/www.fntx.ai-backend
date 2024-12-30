3e46fa99dca33697aa1823a6477659ade8a4ce1c
Ubuntu <ubuntu@ip-172-31-36-81.ap-southeast-1.compute.internal>
2024-12-30 07:30:12 +0000

    Adding code for channels in asgi file.

--- FNTX/asgi.py
+++ FNTX/asgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
+import django
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FNTX.settings")
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
